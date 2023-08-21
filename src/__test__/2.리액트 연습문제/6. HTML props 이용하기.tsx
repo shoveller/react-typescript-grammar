706de86736cb5e0cac3908cca5214ef7b6dd704b
cinos81 <cinos81@gmail.com>
2023-08-22 03:54:12 +0900

    update

--- src/__test__/2.리액트 연습문제/6. HTML props 이용하기.tsx
+++ src/__test__/2.리액트 연습문제/6. HTML props 이용하기.tsx
@@ -1,16 +1,10 @@
-import { ComponentProps, PropsWithChildren } from "react";
-
 /**
  * 아래의 컴포넌트는 자바스크립트의 나머지 프로퍼티 연산자를 사용해서
- * 하지만 타입이 충분하지 않아서 ...rest 에 자동완성이 지원되지 않습니다.
- * ...rest 에 적절한 타입을 입혀 주세요.
+ * 하지만 타입이 충분하지 않아서 모든 props 에 자동완성이 지원되지 않습니다.
+ * unknown 을 대신하는 적절한 타입을 추가해 주세요.
  * <button /> 에 힌트가 있습니다.
  */
-export const 버튼 = ({
-  children,
-  className,
-  ...rest
-}: ComponentProps<"button">) => {
+export const 버튼 = ({ children, className, ...rest }: unknown) => {
   return (
     <button {...rest} className={className}>
       {children}
