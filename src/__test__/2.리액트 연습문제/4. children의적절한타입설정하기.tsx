b8b660bb8d288c2595c5963a35864e1cf34e36b0
cinos81 <cinos81@gmail.com>
2023-08-26 17:13:37 +0900

    재미있나요

--- src/__test__/2.리액트 연습문제/4. children의적절한타입설정하기.tsx
+++ src/__test__/2.리액트 연습문제/4. children의적절한타입설정하기.tsx
@@ -1,7 +1,9 @@
+import React from "react";
+
 type 버튼타입 = {
   // children 에 unknown을 대신할 수 있는 타입을 지정해 주세요.
   // children 은 string, null, number ReactElement 모두가 될 수 있습니다.
-  children: unknown;
+  children: React.ReactNode;
 };
 
 export const 버튼: React.FC<버튼타입> = (props) => {
