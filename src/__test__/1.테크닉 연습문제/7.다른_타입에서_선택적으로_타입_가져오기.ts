ff1e8644e4a4ff39ad1654dd95b8febd520389c3
cinos81 <cinos81@gmail.com>
2023-08-26 16:04:25 +0900

    1-7

--- src/__test__/1.테크닉 연습문제/7.다른_타입에서_선택적으로_타입_가져오기.ts
+++ src/__test__/1.테크닉 연습문제/7.다른_타입에서_선택적으로_타입_가져오기.ts
@@ -1,14 +1,22 @@
 export {};
 
-interface User {
+interface 사람 {
   id: string;
   name: string;
   comment: string;
 }
 
 /**
- * User 인터페이스에서 id 필드와 name 필드만 추출해서 Human 이라는 타입을 만들어 보세요.
+ * 사람 인터페이스에서 id 필드와 name 필드만 추출해서 닝겐 이라는 타입을 만들어 보세요.
  * 키워드는 Omit 과 Pick 입니다.
  * 가능하다면 두가지 방법 모두로 만들어 보세요.
  */
-type Human = unknown;
+type 닝겐1 = { id: string; name: string };
+
+/**
+ * Omit 은 어떤 타입에서 특정 필드만 제거한 새로운 타입을 만듭니다.
+ */
+type 닝겐2 = Omit<사람, "comment">;
+
+// Omit 과 Pick 은 널리 사용하는 타입입니다.
+type 닝겐3 = Pick<사람, "id" | "name">;
