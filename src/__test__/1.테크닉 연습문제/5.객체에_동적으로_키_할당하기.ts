b656a1cca7760ccbc6a368b08b67ee0dedbc9802
cinos81 <cinos81@gmail.com>
2023-08-26 15:46:04 +0900

    에러다루기

--- src/__test__/1.테크닉 연습문제/5.객체에_동적으로_키_할당하기.ts
+++ src/__test__/1.테크닉 연습문제/5.객체에_동적으로_키_할당하기.ts
@@ -1,4 +1,8 @@
-const data = {};
+type 데이터타입 = {
+  [뭐가들어올지모르겠음: string]: string;
+};
+
+const data: 데이터타입 = {};
 
 /**
  * 자바스크립트에서는 가능했던 동적인 키 할당이 동작하지 않습니다.
@@ -8,3 +12,13 @@ const data = {};
 data.a = "1";
 data.b = "2";
 data.dffdfssfd = "dffdfssfd";
+
+/**
+ * 인덱스 시그니쳐를 사용하는 방법
+ * const data: { [id: string]: string } = {}
+ *
+ * 레코드 타입을 사용하는 방법
+ * const data: Record<string, string> = {}
+ *
+ * 둘 다 가능한 방법이고, 꼭 필요한 때가 있습니다.
+ */
--- src/__test__/1.테크닉 연습문제/6.Try-Catch_안의_에러에_타이핑하기.ts
+++ src/__test__/1.테크닉 연습문제/6.Try-Catch_안의_에러에_타이핑하기.ts
@@ -2,13 +2,56 @@
  * 아래의 try ~ catch 문 안에서 타입 에러가 발생하고 있습니다.
  * 지금까지 배운 테크닉 중의 하나를 이용해서 에러를 해결해 보세요.
  * 키워드는 아래와 같습니다.
- * 1. any
- * 2. as
- * 3. instanceof
+ * 1. any // 타입 단정하기 , assertion
+ * 2. as // 타입 단정하기 , assertion
+ * 3. instanceof  // 타입 덜어내기 , narrowing
  */
 
 try {
   // 중요한 일
 } catch (e) {
+  // 자바스크립트는 무엇이든 throw 할 수 있기 때문에
+  // 에러는 항상 unknown
+  // throw ''
+  // throw Promise.resolve('')
   console.log(e.message);
+}
+
+// 답1: any
+try {
+  // 중요한 일
+} catch (e: any) {
+  // 자바스크립트는 무엇이든 throw 할 수 있기 때문에
+  // 에러는 항상 unknown
+  // throw ''
+  // throw Promise.resolve('')
+  console.log(e.message);
+}
+
+// 답2: as
+try {
+  // 중요한 일
+} catch (e) {
+  // 에러가 나면 내가 책임진다!
+  const b = e as Error;
+  // 자바스크립트는 무엇이든 throw 할 수 있기 때문에
+  // 에러는 항상 unknown
+  // throw ''
+  // throw Promise.resolve('')
+  // . 뒤에서 에러터질 확률 up
+  console.log(b.message);
+}
+
+// 답3: instanceof : 추천
+try {
+  // 중요한 일
+} catch (e) {
+  // 자바스크립트는 무엇이든 throw 할 수 있기 때문에
+  // 에러는 항상 unknown
+  // throw ''
+  // throw Promise.resolve('')
+  if (e instanceof Error) {
+    // 에러가 안남
+    console.log(e.message);
+  }
 }
