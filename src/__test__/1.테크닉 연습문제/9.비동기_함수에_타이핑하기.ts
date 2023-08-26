77975eb8a4947be30955146a62564f46339714a5
cinos81 <cinos81@gmail.com>
2023-08-26 16:19:45 +0900

    기본문제

--- src/__test__/1.테크닉 연습문제/9.비동기_함수에_타이핑하기.ts
+++ src/__test__/1.테크닉 연습문제/9.비동기_함수에_타이핑하기.ts
@@ -8,17 +8,24 @@ export {};
  * 가능하다면 콜백함수인터페이스도 완성해 보세요.
  * @link https://www.typescriptlang.org/ko/docs/handbook/2/functions.html
  */
-type 콜백함수타입 = {};
+type 콜백함수타입 = (값: number) => Promise<boolean>;
 
-interface 콜백함수인터페이스 {}
+interface 콜백함수인터페이스 {
+  (값: number): Promise<boolean>;
+}
 
-const 비동기작업 = async (콜백함수) => {
+/**
+ * 1. 자동완성 기능으로 인터페이스 생성
+ * 2. 불필요한 타입 덜어내기
+ * 3. 인라인 어노테이션 잘라내서 인터페이스로 만들기
+ */
+const 비동기작업 = async (콜백함수: 콜백함수인터페이스) => {
   const 실행완료 = await 콜백함수(2);
   if (실행완료) {
     alert("완료되었습니다.");
   }
 };
 
-비동기작업(async (값) => {
+비동기작업(async (값: number) => {
   return 값 === 1;
 });
