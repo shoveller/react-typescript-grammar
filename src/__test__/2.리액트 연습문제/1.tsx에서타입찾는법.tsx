2aa58426492ff1c42e104de6bdc9e9d75e4b9322
cinos81 <cinos81@gmail.com>
2023-08-26 16:42:34 +0900

    tsx 연습

--- src/__test__/2.리액트 연습문제/1.tsx에서타입찾는법.tsx
+++ src/__test__/2.리액트 연습문제/1.tsx에서타입찾는법.tsx
@@ -1,12 +1,20 @@
+import { AriaRole, MouseEventHandler } from "react";
+
 /**
  * dom 엘리먼트의 정확한 타입을 찾아 보세요.
  */
 export const 버튼 = () => {
   // role prop 에 적합한 타입은 무엇일까요?
-  const role: unknown = undefined;
+  // AriaRole 은 string을 포함하는 타입이라 string 도 가능
+  // | undefined 있을수도 있고 없을수도 있다
+  const role: AriaRole | undefined = "button";
 
   // onClick prop 에 적합한 타입은 무엇일까요?
-  const onClick: unknown = (e) => {};
+  // | undefined 있을수도 있고 없을수도 있다
+  // () => 도 허용한다
+  const onClick: MouseEventHandler<HTMLButtonElement> | undefined = (e) => {
+    e.detail;
+  };
 
   return <button role={role} onClick={onClick} />;
 };
